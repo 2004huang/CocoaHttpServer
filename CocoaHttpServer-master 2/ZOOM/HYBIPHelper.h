48e2ebe2a3c6ef3476dfb8bcdf85983e9190f9b7
2004huang <200418030@163.com>
2018-11-09 17:17:57 +0800

    httpSever 搭建手机本地服务器

---
+++ CocoaHttpServer-master 2/ZOOM/HYBIPHelper.h
@@ -0,0 +1,15 @@
+//
+//  HYBIPHelper.h
+//  ZOOM
+//
+//  Created by yalihuang on 2018/11/9.
+//  Copyright © 2018年 Weshape3D. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HYBIPHelper : NSObject
+
++ (NSString *)deviceIpAdress;
+
+@end
